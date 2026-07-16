c0bc7130e77c0f6029bfc8eaf4772dc13fb94ad5
dazzy-ji <d.indimuli@alustudent.com>
2026-07-16 18:45:51 +0200

    spelling errors corrected

--- alu_db.sql
+++ alu_db.sql
@@ -20,13 +20,14 @@ CREATE Table Students(
     name VARCHAR(100),
     email VARCHAR(100),
     classroom_id INT,
+    Foreign Key (classroom_id) REFERENCES classroom(classroom_id),
     enrollment_date DATE
 );
 
 INSERT INTO Students(student_id, name, email, classroom_id, enrollment_date) VALUES
 (001, 'Dazzy Indimuli', 'd.indimuli@alustudent.com', 101, '2026-01-07'),
 (002, 'David Ngure', 'd.wanjiku@alustudent.com', 101, '2026-01-10'),
-(003, 'Isaack Kariuki', 'i.kariuki@alustudent.com', 102, '2026-01-08'),
-(004, 'Philip Otieno', 'p.otieno@alustudent.com', 102, '2026-01-09'),
-(005, 'Arif Antulay', 'a.antulay@alustudent.com', 103, '2026-01-10'),
-(006, 'Eddy Mutoniwase', 'e.mutoniwas@alustudent.com', 103, '2026-01-06');
+(003, 'Isaack Kariuki', 'i.kariuki@alustudent.com', 103, '2026-01-08'),
+(004, 'Philip Otieno', 'p.otieno@alustudent.com', 104, '2026-01-09'),
+(005, 'Arif Antulay', 'a.antulay@alustudent.com', 102, '2026-01-10'),
+(006, 'Eddy Mutoniwase', 'e.mutoniwas@alustudent.com', 105, '2026-01-06');
