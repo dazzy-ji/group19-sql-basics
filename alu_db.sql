111ec00371fc5f519534ef1fb0532c8d517d2955
dwanjiku1-lgtm <d.wanjiku1@alustudent.com>
2026-07-16 18:32:33 +0200

    classroom_table created

--- alu_db.sql
+++ alu_db.sql
@@ -1,1 +1,17 @@
 CREATE DATABASE ALU_DATABASE;
+CREATE TABLE classroom(
+    classroom_id INT PRIMARY KEY,
+    room_number VARCHAR(10),
+    building VARCHAR(50),
+    capacity INT
+);
+INSERT INTO classroom(classroom_id , room_number ,building ,capacity )
+VALUES
+(101, '1a ', 'KENYA', 100),
+(102, '2b ', 'EGYPT', 55),
+(103, '3c', 'RWANDA', 80),
+(104, '4d', 'NIGERIA', 105),
+(105, '5e', 'TANZANIA', 20);
+
+
+
